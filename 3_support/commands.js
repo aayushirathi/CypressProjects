be7511f2ada0fc3807b0558f64bf90e6f036984b
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2026-01-02 18:49:14 +0530

    Add files via upload

---
+++ 3_support/commands.js
@@ -0,0 +1,46 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+/// <reference types="cypress" /> 
+/// <reference types="cypress-xpath" />
+
+
+Cypress.Commands.add("getIframe",(iframe)=>{
+    return cy.get(iframe)
+    .its("0.contentDocument.body")
+    .should("be.visible")
+    .then(cy.wrap);
+})
+
+Cypress.Commands.add("clickLink",(label)=>{
+    cy.get('a').contains(label).click()
+})
+
+Cypress.Commands.add("loginapp",(username,password)=>{
+    cy.get("input[placeholder='Username']").type(username)
+    cy.get("input[placeholder='Password']").type(password)
+    cy.get("button[type='submit']").click()
+})
